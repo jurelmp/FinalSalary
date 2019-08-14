eb7329c18669179fd109bb59605af8b26509ebcf
Jurel P <patocjurel@gmail.com>
2019-08-14 22:07:56 +0800

    Setup test script

---
+++ src/test/java/ph/petrologisticscorp/finalsalary/ExampleTests.java
@@ -0,0 +1,13 @@
+package ph.petrologisticscorp.finalsalary;
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+class ExampleTests {
+
+    @Test
+    void example() {
+        assertTrue(true);
+    }
+}
